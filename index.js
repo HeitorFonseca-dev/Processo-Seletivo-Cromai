1c5e57aab633a98c0cb8d457ed9dd12efaa42bb8
Heitor Ramos <86064837+xHec@users.noreply.github.com>
2023-01-23 21:09:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
-    function calcular() {
+    function calcular(CatetoOposto) {
         var valorHipotenusa = document.getElementById("inputHipotenusa").value;
         var valorCatetoOposto = document.getElementById("inputCatetoOposto").value;
         var valorCatetoAdjascente = document.getElementById("inputCatetoAdjascente").value;
-
-        valorHipotenusa = Math.pow(valorCatetoOposto, 2) + Math.pow(valorCatetoAdjascente, 2);
-
-      
+        
+        valorHipotenusa = Math.hypot(valorCatetoOposto, valorCatetoAdjascente,valorHipotenusa);
 
         document.getElementById("resultado").value = valorHipotenusa;
-
-         console.log (Resultado)
+                      
     }
+
+
 
 
 
