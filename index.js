e0c249257e71b40a551fe1bced5ecd0722305c49
Heitor Ramos <86064837+xHec@users.noreply.github.com>
2023-01-25 14:54:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 
-    function calcular() {
-        var valorHipotenusa = document.getElementById("inputHipotenusa").value;
-        var valorCatetoOposto = document.getElementById("inputCatetoOposto").value;
-        var valorCatetoAdjascente = document.getElementById("inputCatetoAdjascente").value;
-        
-        valorHipotenusa = Math.hypot(valorCatetoOposto, valorCatetoAdjascente,valorHipotenusa);
+function calcular() {
+    let valorHipotenusa = document.getElementById("inputHipotenusa").value;
+    let valorCatetoOposto = document.getElementById("inputCatetoOposto").value;
+    let valorCatetoAdjacente = document.getElementById("inputCatetoAdjacente").value;
 
-        document.getElementById("resultado").value = valorHipotenusa;
-                      
+    valorHipotenusa = valorHipotenusa ? valorHipotenusa : 1.0;
+    valorCatetoOposto = valorCatetoOposto ? valorCatetoOposto : 1.0;
+    valorCatetoAdjacente = valorCatetoAdjacente ? valorCatetoAdjacente : 1.0;
+
+    fetch('http://127.0.0.1:5000/hipotenusa/' + parseFloat(valorCatetoAdjacente).toFixed(1) + '/' + parseFloat(valorCatetoOposto).toFixed(1) + '/' + parseFloat(valorHipotenusa).toFixed(1) + '/')
+        .then((response) => response.json())
+        .then((data) => document.getElementById("resultado").value = data);
+}
+
+$(document).on("click", "#calcular", function () {
+
+    var quantidadePreenchido = $('.opcao:text').filter(function (input) {
+        return $(this).val() != "";
+    }).length;
+
+    if (quantidadePreenchido != 2) {
+        alert("Preencha dois campos");
+    } else {
+        calcular();
     }
-
-
-
-
-
+});
